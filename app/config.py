743261484904f8c320e1770230ea1a56494ed48f
Imad Ahmad <imadahmad97@yahoo.ca>
2025-07-11 23:02:24 -0700

    removed oauth

---
+++ app/config.py
@@ -0,0 +1,16 @@
+import os
+import app
+
+app.config["OAUTH2_PROVIDERS"] = {
+    "google": {
+        "client_id": os.environ.get("GOOGLE_CLIENT_ID"),
+        "client_secret": os.environ.get("GOOGLE_CLIENT_SECRET"),
+        "authorize_url": "https://accounts.google.com/o/oauth2/auth",
+        "token_url": "https://accounts.google.com/o/oauth2/token",
+        "userinfo": {
+            "url": "https://www.googleapis.com/oauth2/v3/userinfo",
+            "email": lambda json: json["email"],
+        },
+        "scopes": ["https://www.googleapis.com/auth/userinfo.email"],
+    }
+}
